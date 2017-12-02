1203ec2d2ff6c3e5ffb37d0bd01f32d110e7e353
Ali <alicheddadi@gmail.com>
2017-12-01 23:21:44 -0500

    Scene 3 WIP, somewhat satisfied with spike enemy.

--- Assets/Scripts/Enemies/SpikeControl.cs
+++ Assets/Scripts/Enemies/SpikeControl.cs
@@ -7,12 +7,29 @@ public class SpikeControl : MonoBehaviour
     private float _initialX;
     private int _direction;
 
-    public bool enableSway = false, reverseDirection = false;
+    public bool enableSway = false, reverseDirection = false, randomPosition = false;
     public float swayDistance = 0.5f, swayDelay = 10.0f;
 
     private void Start()
     {
+        if (reverseDirection) _direction = -1;
+        else _direction = 1;
         _initialX = transform.position.x;
+        if (randomPosition)
+        {
+            CircleCollider2D circle = GetComponent<CircleCollider2D>();
+            Collider2D collider;
+            do
+            {
+                if (!enableSway) _initialX = Random.Range(-2.5f, 2.5f);
+                else
+                {
+                    if (_direction > 0) _initialX = Random.Range(-2.5f, 2.5f - swayDistance);
+                    else _initialX = Random.Range(-2.5f + swayDistance, 2.5f);
+                }
+                collider = Physics2D.OverlapCircle(new Vector2(_initialX, transform.position.y), circle.radius);
+            } while (collider != null);
+        }
     }
 
     private void Update()
