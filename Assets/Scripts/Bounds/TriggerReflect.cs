6685a6443852976e0b089bab5fde2bfb7af54747
acheddadi <alicheddadi@gmail.com>
2017-12-11 17:35:06 -0500

    Redesigned all levels to lower the difficulty significantly.

--- Assets/Scripts/Bounds/TriggerReflect.cs
+++ Assets/Scripts/Bounds/TriggerReflect.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class TriggerReflect : MonoBehaviour
 {
+    private float _timer;
+
+    public float reflectDelay = 0.5f;
+
+    private void Start()
+    {
+        _timer = Time.time;
+    }
+
     // Change direction when bounds are touched
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        PlayerControl player = collision.GetComponent<PlayerControl>();
-        if (player != null) player.ChangeDirection();
+        if (Time.time > _timer + reflectDelay)
+        {
+            PlayerControl player = collision.GetComponent<PlayerControl>();
+            if (player != null && !collision.isTrigger) player.ChangeDirection();
+            _timer = Time.time;
+        }
+        
     }
 }
